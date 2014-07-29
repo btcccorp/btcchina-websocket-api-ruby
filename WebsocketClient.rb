c32cb6ea277b10d6c3cf6dbf34cb5f155789eea6
guoqing <guoqing.yu@btcchina.com>
2014-07-29 16:39:14 +0800

    change debug info

--- WebsocketClient.rb
+++ WebsocketClient.rb
@@ -5,18 +5,18 @@ require 'socket.io-client-simple'
 socket = SocketIO::Client::Simple.connect 'https://websocket.btcchina.com'
 
 socket.on :connect do
-  puts "connected!"
+  puts "Connected!"
   socket.emit :subscribe, "marketdata_cnybtc"
   socket.emit :subscribe, "marketdata_cnyltc"
   socket.emit :subscribe, "marketdata_btcltc"
 end
 
 socket.on :disconnect do
-  puts "disconnected!"
+  puts "Disconnected!"
 end
 
 socket.on :trade do |data|
-  puts 'trade:'
+  puts 'New Trade:'
   p data
 end
 
